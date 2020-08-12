848070e4aa09a8c89dd5b1a945bd86cf8e940d02
Ignacio Lizarralde <ijlizarralde@gmail.com>
2020-08-12 19:26:33 -0300

    module4 assignment

---
+++ module4-solution/script.js
@@ -0,0 +1,17 @@
+(function() {
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i in names) {
+  
+    var firstLetter = names[i].charAt(0);
+    
+    if (firstLetter == 'j' || firstLetter == 'J') {
+      window.byeSpeaker.speak(names[i]);
+    } else {
+      window.helloSpeaker.speak(names[i]);
+    }
+
+  }
+
+})();
